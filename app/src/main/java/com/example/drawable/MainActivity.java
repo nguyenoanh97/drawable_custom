c8f70ac598f9467f21d0403a45f7bd91024318eb
oanh0444@gmail.com <anhnguyetsol97@gmail.com>
2021-05-07 17:11:59 +0700

    clip custom

--- app/src/main/java/com/example/drawable/MainActivity.java
+++ app/src/main/java/com/example/drawable/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.drawable;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.ClipDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imgMonAn;
+    Button btnClip;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imgMonAn = (ImageView) findViewById(R.id.imageViewMonAn);
+        btnClip = (Button) findViewById(R.id.button_clip) ;
+
+        imgMonAn.setImageLevel(5000);
+
+        ClipDrawable clipdrawable = (ClipDrawable) imgMonAn.getDrawable();
+
+        btnClip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(clipdrawable.getLevel()>=10000) {
+
+                }
+                imgMonAn.setImageLevel(clipdrawable.getLevel() + 1000);
+
+            }
+        });
     }
 }
